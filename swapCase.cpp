591830ec71a459f42d09071acb5565532ac7f2be
Swasti-23 <117920342+Swasti-23@users.noreply.github.com>
2023-01-26 14:54:53 +0530

    Swap case

---
+++ swapCase.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    //SwapCase without inbuilt function
+    for(int i=0;i<str.size();i++){
+        if(str[i]>='a' && str[i]<='z'){
+            str[i]-=32;
+        }
+        else if(str[i]>='A' && str[i]<='Z'){
+            str[i]+=32;
+        }
+    }
+    cout<<str;
+    return 0;
+}
