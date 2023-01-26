a221a5e2c4930aa7bfc9a05593060005b2daa2d4
Swasti-23 <117920342+Swasti-23@users.noreply.github.com>
2023-01-26 15:10:13 +0530

    Maximum freq of a letter in a string

---
+++ maxFreq.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include<string>
+using namespace std;
+int main(){
+    string s="wioanmviansdnc";
+    int freq[26];
+    for(int i=0;i<26;i++){
+        freq[i]=0;
+    }
+    for(int i=0;i<s.size();i++){
+        freq[s[i]-'a']++;
+    }
+    char ans='a';
+    int maxF=0;
+    for(int i=0;i<26;i++){
+        if(freq[i]>maxF){
+            maxF=freq[i];
+            ans=i+'a';
+        }
+    }
+    cout<<maxF<<" "<<ans;
+    return 0;
+}
