7b737dfb4e448855f0d8c376f9d11afd04c4353f
Swasti-23 <117920342+Swasti-23@users.noreply.github.com>
2023-01-28 00:01:06 +0530

    reverse a string

---
+++ recursion/reverse.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include<string>
+using namespace std;
+void reverse(string s){
+    if(s.length()==0){
+        return;
+    }
+    string ros=s.substr(1);
+    reverse(ros);
+    cout<<s[0];
+}
+int main(){
+   reverse("binod");
+    return 0;
+}
