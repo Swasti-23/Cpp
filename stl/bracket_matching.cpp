ac5575bb0c6b2607c54f4600d4dff8717b4a32b0
Swasti-23 <117920342+Swasti-23@users.noreply.github.com>
2023-03-06 22:09:01 +0530

    bracket matching

---
+++ stl/bracket_matching.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+unordered_map<char,int> symbols={{'[',-1},{'(',-2},{'{',-3},{']',1},{')',2},{'}',3}};
+
+string isBalanced(string s){
+    stack<char> st;
+    for(char bracket:s){
+        if(symbols[bracket]<0){
+            st.push(bracket);
+        }else{
+            if(st.empty()) return 0;
+            char top=st.top();
+            st.pop();
+            if(symbols[top]+symbols[bracket]!=0){
+                return "NO";
+            }
+        }
+    }
+    if(st.empty()) return "YES";
+    return "NO";
+}
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        cout<<isBalanced(s)<<endl;
+    }
+	return 0;
+}
